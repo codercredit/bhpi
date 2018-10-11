2dec4f2cd6e4bfe4bd6f639fa0f3a6e0ef9c7740
md_mehadi_hassan <shilonmahadi7@gmail.com>
2018-10-11 18:45:45 +0600

    devmmhs
    Author

---
+++ js/main.js
@@ -0,0 +1,218 @@
+(function ($) {
+    "use strict";
+
+
+
+    // One Page Nav
+    var top_offset = $('.header-area').height() - 10;
+    $('.main-menu nav ul').onePageNav({
+        currentClass: 'active',
+        scrollOffset: top_offset,
+    });
+
+
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll < 245) {
+            $(".header-sticky").removeClass("sticky");
+        } else {
+            $(".header-sticky").addClass("sticky");
+        }
+    });
+
+
+
+    // mainSlider
+    function mainSlider() {
+        var BasicSlider = $('.slider-active');
+        BasicSlider.on('init', function (e, slick) {
+            var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+            doAnimations($firstAnimatingElements);
+        });
+        BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+            var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+            doAnimations($animatingElements);
+        });
+        BasicSlider.slick({
+            autoplay: false,
+            autoplaySpeed: 10000,
+            dots: false,
+            fade: true,
+            prevArrow: "<button type='button' class='slick-prev pull-left'><span class='left-arrow-main-slider'><i class='fas fa-angle-left'></i></span></button>",
+            nextArrow: "<button type='button' class='slick-next pull-right'><span class='right-arrow-main-slider'><i class='fas fa-angle-right'></i></span></button>",
+            arrows: true,
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        dots: false,
+                        arrows: false
+                    }
+                }
+		]
+        });
+
+        function doAnimations(elements) {
+            var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            elements.each(function () {
+                var $this = $(this);
+                var $animationDelay = $this.data('delay');
+                var $animationType = 'animated ' + $this.data('animation');
+                $this.css({
+                    'animation-delay': $animationDelay,
+                    '-webkit-animation-delay': $animationDelay
+                });
+                $this.addClass($animationType).one(animationEndEvents, function () {
+                    $this.removeClass($animationType);
+                });
+            });
+        }
+    }
+    mainSlider();
+
+    // testimonilas-activation
+    $('.single-content-activation').slick({
+        dots: false,
+        arrows: true,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><span class='left-arrow-main-slider'><i class='fas fa-caret-left'></i></span></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><span class='right-arrow-main-slider'><i class='fas fa-caret-right'></i></span></button>",
+        infinite: true,
+        speed: 600,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    arrows: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+			},
+            {
+                breakpoint: 600,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+			},
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
+    });
+
+    // owlCarousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            767: {
+                items: 3
+            },
+            992: {
+                items: 5
+            }
+        }
+    })
+
+
+    /* magnificPopup img view */
+    $('.popup-image').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    /* magnificPopup video view */
+    $('.popup-video').magnificPopup({
+        type: 'iframe'
+    });
+
+
+    // isotop
+    $('.grid').imagesLoaded(function () {
+        // init Isotope
+        var $grid = $('.grid').isotope({
+            itemSelector: '.grid-item',
+            percentPosition: true,
+            masonry: {
+                // use outer width of grid-sizer for columnWidth
+                columnWidth: '.grid-item',
+            }
+        });
+    });
+
+    // filter items on button click
+    $('.portfolio-menu').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+
+    //for menu active class
+    $('.portfolio-menu button').on('click', function (event) {
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        event.preventDefault();
+    });
+
+
+    //    loadmore
+
+
+    $(function () {
+        $(".box-hidden").slice(0, 3).show();
+        
+        $("#loadMore").on('click', function (e) {
+            e.preventDefault();
+            $(".box-hidden:hidden").slice(0, 3).slideDown();
+            if ($(".box-hidden:hidden").length == 0) {
+                $("#load").fadeOut('slow');
+            }
+            $('html,body').animate({
+                scrollTop: $(this).offset().top
+            }, 1500);
+        });
+    });
+
+
+
+
+    // scrollToTop
+    $.scrollUp({
+        scrollName: 'scrollUp', // Element ID
+        topDistance: '300', // Distance from top before showing element (px)
+        topSpeed: 300, // Speed back to top (ms)
+        animation: 'fade', // Fade, slide, none
+        animationInSpeed: 200, // Animation in speed (ms)
+        animationOutSpeed: 200, // Animation out speed (ms)
+        scrollText: '<i class="icofont icofont-long-arrow-up"></i>', // Text for element
+        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+    });
+
+    // WOW active
+    new WOW().init();
+
+
+})(jQuery);
